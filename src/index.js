609bedcd302808d7abd55813d63e1ca1853eae8d
Jaykingamez <jaykinchan@gmail.com>
2022-08-16 16:26:56 +0800

    code splitting

--- src/index.js
+++ src/index.js
@@ -1,28 +1,14 @@
-import _ from 'lodash';
-import printMe from './print.js';
 
-function component() {
+async function getComponent() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
+    const { default: _ } = await import('lodash');
+
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;  // onclick event is bind to the original printMe function
-
-    element.appendChild(btn);
 
     return element;
 }
 
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
-
-if (module.hot) {
-    module.hot.accept('./print.js', function () {
-        console.log('Accepting the updated printMe module!');
-        document.body.removeChild(element);
-        element = component(); // Re-render the "component" to update the click handler
-        document.body.appendChild(element);
-    })
-}
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});
